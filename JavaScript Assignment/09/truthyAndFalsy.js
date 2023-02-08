aa6108b8832f7a17d2cc2aeaa34b10d92160a0db
krrishmittal <mittalkrrish0811@gmail.com>
2023-02-08 21:35:00 +0530

    Q10 solved

--- JavaScript Assignment/09/truthyAndFalsy.js
+++ JavaScript Assignment/09/truthyAndFalsy.js
@@ -1,6 +1,3 @@
-//09. Boolean value is either true or false.
-//- Write three JavaScript statement example which provide truthy value.
-//- Write three JavaScript statement example which provide falsy value.
 //1st example true value
 let statement = true;
 //2nd example true value
---
+++ JavaScript Assignment/10/trueFalse.js
@@ -0,0 +1,30 @@
+// 4 > 3: True
+// 4 >= 3: True
+// 4 < 3: False
+// 4 <= 3: False
+// 4 == 4: True
+// 4 === 4: True
+// 4 != 4: False
+// 4 !== 4: False
+// 4 != '4': False
+// 4 == '4': True
+// 4 === '4': False
+
+console.log(4 > 3); // true
+console.log(4 >= 3); // true
+console.log(4 < 3); // false
+console.log(4 <= 3); // false
+console.log(4 == 4); // true
+console.log(4 === 4); // true
+console.log(4 != 4); // false
+console.log(4 !== 4); // false
+console.log(4 != '4'); // false
+console.log(4 == '4'); // true
+console.log(4 === '4'); // false
+
+let python = "Python";
+let jargon = "Jargon";
+
+console.log(python.length) 
+console.log(jargon.length) 
+console.log(python.length != jargon.length);
